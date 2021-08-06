e5a2439f62ae22b8816abb797a50af5d923c65ae
karyshev_m <karyshev63rus@gmail.com>
2021-08-06 12:23:02 +0400

    created func create_order_items_list

--- orders/views.py
+++ orders/views.py
@@ -15,51 +15,16 @@ TRANSPORT_EXTRA_COST_RATE = 1.5
 
 def order_create(request):
     cart = get_cart(request)
-    # cart_qty = sum(item['quantity'] for item in cart.values())
-    # transport_cost = str(round((TRANSPORT_BASE_COST + (cart_qty // 10) * TRANSPORT_EXTRA_COST_RATE), 2))
     transport_cost = get_transport_cost(request, cart)
     if request.method == 'POST':
-        order_form = OrderCreateForm(request.POST)
-        if order_form.is_valid():
-            cf = order_form.cleaned_data
-            transport = cf['transport']
-            if transport == 'самовывоз':
-                transport_cost = 0
-        order = order_form.save(commit=False)
-        if request.user.is_authenticated:
-            order.user = request.user
-        order.transport_cost = Decimal(transport_cost)
-        order.save()
-        product_ids = cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
-        for product in products:
-            cart_item = cart[str(product.id)]
-            OrderItem.objects.create(
-                order=order,
-                product=product,
-                price=cart_item['price'],
-                quantity=cart_item['quantity']
-            )
+        order = save_valid_order_form(request, transport_cost)
+        create_order_items_list(cart, order)
         cart_clear(request)
         order_created.delay(order.id)
         return render(request,
                       'order_created.html',
                       {'order': order})
-    else:
-        order_form = get_order_form(request)
-        # order_form = OrderCreateForm()
-        # if request.user.is_authenticated:
-        #     initial_data = {
-        #         'first_name': request.user.first_name,
-        #         'last_name': request.user.last_name,
-        #         'email': request.user.email,
-        #         'telephone': request.user.profile.phone_number,
-        #         'address': request.user.profile.address,
-        #         'postal_code': request.user.profile.postal_code,
-        #         'city': request.user.profile.city,
-        #         'country': request.user.profile.country,
-        #     }
-        #     order_form = OrderCreateForm(initial=initial_data)
+    order_form = get_order_form(request)
     return render(request,
                   'order_create.html',
                   {'cart': cart,
@@ -71,6 +36,34 @@ def get_transport_cost(request, cart):
     cart_qty = sum(item['quantity'] for item in cart.values())
     transport_cost = str(round((TRANSPORT_BASE_COST + (cart_qty // 10) * TRANSPORT_EXTRA_COST_RATE), 2))
     return transport_cost
+
+
+def save_valid_order_form(request, transport_cost):
+    order_form = OrderCreateForm(request.POST)
+    if order_form.is_valid():
+        cf = order_form.cleaned_data
+        transport = cf['transport']
+        if transport == 'самовывоз':
+            transport_cost = 0
+    order = order_form.save(commit=False)
+    if request.user.is_authenticated:
+        order.user = request.user
+    order.transport_cost = Decimal(transport_cost)
+    order.save()
+    return order
+
+
+def create_order_items_list(cart, order):
+    product_ids = cart.keys()
+    products = Product.objects.filter(id__in=product_ids)
+    for product in products:
+        cart_item = cart[str(product.id)]
+        OrderItem.objects.create(
+            order=order,
+            product=product,
+            price=cart_item['price'],
+            quantity=cart_item['quantity']
+        )
 
 
 def get_order_form(request):
