583e38c403bc1c602215244062ee249b667ab628
karyshev_m <karyshev63rus@gmail.com>
2021-08-06 11:45:28 +0400

    created funcs: get_transport_cost & get_order_form

--- orders/views.py
+++ orders/views.py
@@ -9,11 +9,15 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 import weasyprint
 
+TRANSPORT_BASE_COST = 3.99
+TRANSPORT_EXTRA_COST_RATE = 1.5
+
 
 def order_create(request):
     cart = get_cart(request)
-    cart_qty = sum(item['quantity'] for item in cart.values())
-    transport_cost = str(round((3.99 + (cart_qty // 10) * 1.5), 2))
+    # cart_qty = sum(item['quantity'] for item in cart.values())
+    # transport_cost = str(round((TRANSPORT_BASE_COST + (cart_qty // 10) * TRANSPORT_EXTRA_COST_RATE), 2))
+    transport_cost = get_transport_cost(request, cart)
     if request.method == 'POST':
         order_form = OrderCreateForm(request.POST)
         if order_form.is_valid():
@@ -42,24 +46,48 @@ def order_create(request):
                       'order_created.html',
                       {'order': order})
     else:
-        order_form = OrderCreateForm()
-        if request.user.is_authenticated:
-            initial_data = {
-                'first_name': request.user.first_name,
-                'last_name': request.user.last_name,
-                'email': request.user.email,
-                'telephone': request.user.profile.phone_number,
-                'address': request.user.profile.address,
-                'postal_code': request.user.profile.postal_code,
-                'city': request.user.profile.city,
-                'country': request.user.profile.country,
-            }
-            order_form = OrderCreateForm(initial=initial_data)
+        order_form = get_order_form(request)
+        # order_form = OrderCreateForm()
+        # if request.user.is_authenticated:
+        #     initial_data = {
+        #         'first_name': request.user.first_name,
+        #         'last_name': request.user.last_name,
+        #         'email': request.user.email,
+        #         'telephone': request.user.profile.phone_number,
+        #         'address': request.user.profile.address,
+        #         'postal_code': request.user.profile.postal_code,
+        #         'city': request.user.profile.city,
+        #         'country': request.user.profile.country,
+        #     }
+        #     order_form = OrderCreateForm(initial=initial_data)
     return render(request,
                   'order_create.html',
                   {'cart': cart,
                    'order_form': order_form,
                    'transport_cost': transport_cost})
+
+
+def get_transport_cost(request, cart):
+    cart_qty = sum(item['quantity'] for item in cart.values())
+    transport_cost = str(round((TRANSPORT_BASE_COST + (cart_qty // 10) * TRANSPORT_EXTRA_COST_RATE), 2))
+    return transport_cost
+
+
+def get_order_form(request):
+    order_form = OrderCreateForm()
+    if request.user.is_authenticated:
+        initial_data = {
+            'first_name': request.user.first_name,
+            'last_name': request.user.last_name,
+            'email': request.user.email,
+            'telephone': request.user.profile.phone_number,
+            'address': request.user.profile.address,
+            'postal_code': request.user.profile.postal_code,
+            'city': request.user.profile.city,
+            'country': request.user.profile.country,
+        }
+        order_form = OrderCreateForm(initial=initial_data)
+    return order_form
 
 
 def invoice_pdf(request, order_id):
