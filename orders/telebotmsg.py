e5b6974163011e56d93a5649ce1d5dd9a6d01da6
karyshev_m <karyshev63rus@gmail.com>
2021-08-07 21:27:17 +0400

    modified text in telegrambot function

--- orders/telebotmsg.py
+++ orders/telebotmsg.py
@@ -11,8 +11,8 @@ def sent_telegram_messages(order_id, order_status, first_name,
     requests.post(method, data={
             'chat_id': tg_chat_id,
             'text': f'Заказ № {order_id}\n'
-                    f'на сумму {total_cost} д.е.\n'
-                    f'перевен в статус "{order_status}".\n'
+                    f'на сумму {total_cost} руб.\n'
+                    f'получил статус "{order_status}".\n'
                     f'Покупатель: {first_name} {last_name}\n'
                     f'тел.: {telephone}\n'
                     f'эл.почта: {email}'
