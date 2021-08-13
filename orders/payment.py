c5d6ab44626c21eafceeb156bec4fccd719e5be4
karyshev_m <karyshev63rus@gmail.com>
2021-08-13 06:49:26 +0400

    tuned a payment url link

--- orders/payment.py
+++ orders/payment.py
@@ -6,7 +6,7 @@ from .models import Order, Payment
 from .tasks import status_change_notification
 from rest_framework import generics, serializers
 
-URL = 'http://localhost:8000/orders/api/'
+PAYMENT_URL = settings.PAYMENT_URL
 
 
 def order_pay_link(request, order_id):
@@ -16,7 +16,7 @@ def order_pay_link(request, order_id):
             'order_total_cost': order.get_total_cost,
             'link_for_back_redirect_to_shop':
                 f'https://docent63.ru/orders/order/pay/confirm/{order_id}/'}
-    response = requests.get(URL, data)
+    response = requests.get(PAYMENT_URL, data)
     link = response.json()[0]['link']
     order_pay_link_url = f'{link}{order_id}'
     return HttpResponseRedirect(order_pay_link_url)
